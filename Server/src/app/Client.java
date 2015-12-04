dd8fa6f1491c3f92cd05a93a3700d3f3c736f525
IATIC5-Cyril <kampfer@live.fr>
2015-12-04 11:04:35 +0100

    Debut mise en place du serveur

---
+++ Server/src/app/Client.java
@@ -0,0 +1,60 @@
+package app;
+
+import utils.socket.DataArrivalEvent;
+import utils.socket.SClient;
+import utils.socket.SClientAdapter;
+import utils.socket.SClientListener;
+import utils.socket.message.ErrorMessage;
+import messages.PingRequest;
+import messages.PingResponse;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.HashMap;
+
+public class Client {
+    private final HashMap<Class, IMessageCallback> messagesCallback = new HashMap<>();
+    private SClient socket;
+
+    public interface IMessageCallback {
+        void run(DataArrivalEvent e);
+    }
+
+    public Client(Socket socket) {
+        try {
+            this.registerCallback();
+            this.socket = new SClient(socket, clientEvent);
+        } catch (IOException e) {
+            System.err.println("[Serveur] Erreur création du client.");
+            e.printStackTrace();
+        }
+    }
+
+    public SClientListener clientEvent = new SClientAdapter() {
+        @Override
+        public void onDataArrival(SClient sender, DataArrivalEvent event) {
+            //TODO: Si non connecté & non LoginRequest, retourne une erreur ?
+            if(messagesCallback.containsKey(event.getMessageClass()))
+                messagesCallback.get(event.getMessageClass()).run(event);
+            else if(event.isRequest())
+                event.setResponse(new ErrorMessage(0, "Request not found"));
+        }
+
+        @Override
+        public void onClosed(SClient sender) {
+            System.out.println("[Serveur] Deconnexion du client");
+        }
+    };
+
+
+    ///////////////// Gestion des messages ///////////////
+
+    private void registerCallback() {
+        messagesCallback.put(PingRequest.class, onPingRequest);
+    }
+
+    public IMessageCallback onPingRequest = data -> {
+        data.setResponse(new PingResponse());
+    };
+
+}
--- Server/src/app/Main.java
+++ Server/src/app/Main.java
@@ -1,9 +1,40 @@
 package app;
 
+import utils.socket.SClient;
+import utils.socket.Server;
+import utils.socket.ServerListener;
+import messages.PingRequest;
+
+import java.io.IOException;
+
 public class Main {
 
+    public static Server server = new Server();
+
     public static void main(String[] args) {
-        System.out.println("Server");
+        try {
+            System.out.println("Démarrage de l'application ...");
+
+            //TODO: JDBC - Connexion MySQL
+
+            // Configuration du serveur //
+            server.addListener(onNewConnection);
+            server.listen(3698);
+
+            //TODO>> Debug
+            SClient oo = new SClient("127.0.0.1",3698);
+            System.out.println(oo.sendResponse(new PingRequest()));
+            //TODO<<
+        }
+        catch (IOException e) {
+            System.err.println("Erreur d'initialisation !");
+            e.printStackTrace();
+            System.exit(0);
+        }
     }
 
+    public static ServerListener onNewConnection = (sender, socket) -> {
+        // Nouvelle connexion //
+        new Client(socket);
+    };
 }
