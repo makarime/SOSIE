d4edcd5d226fe048c0a85a6344a6cf62f5071856
IATIC5-Cyril <kampfer@live.fr>
2015-12-07 11:31:36 +0100

    Ajout du message "ClassStudent"

---
+++ Common/src/messages/ClassStudentRequest.java
@@ -0,0 +1,15 @@
+package messages;
+
+import utils.socket.IMessage;
+
+public class ClassStudentRequest implements IMessage{
+    public final int classId;
+
+    public ClassStudentRequest(int classId) {
+        this.classId = classId;
+    }
+
+    public int getClassId() {
+        return classId;
+    }
+}
---
+++ Common/src/messages/ClassStudentResponse.java
@@ -0,0 +1,18 @@
+package messages;
+
+import Models.Student;
+import utils.socket.IMessage;
+
+import java.util.ArrayList;
+
+public class ClassStudentResponse implements IMessage{
+    public final ArrayList<Student> students;
+
+    public ClassStudentResponse(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+}
